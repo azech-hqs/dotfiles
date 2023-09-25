bf7131cd9fdcd7b977fe5248fcf6c59e9658866d
Alexander Zech <alexzech@gmx.de>
2023-09-24 23:40:27 -0700

    feat: configure lualine

--- nvim/lua/plugins/imports/editor.lua
+++ nvim/lua/plugins/imports/editor.lua
@@ -21,5 +21,19 @@ return {
         config = function()
             require("plugins.configs.telescope")
         end,
-    }
+    },
+
+    -- Set lualine as statusline
+    {
+        'nvim-lualine/lualine.nvim',
+        -- See `:help lualine.txt`
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = 'onedark',
+                component_separators = '|',
+                section_separators = '',
+            },
+        },
+    },
 }
