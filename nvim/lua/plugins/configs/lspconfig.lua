4fdc21905dd6d0da8e2b241f2d325cc32d43f969
Alexander Zech <alexzech@gmx.de>
2023-10-04 23:07:55 -0700

    update: add rust_analyzer to language servers

--- nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/lua/plugins/configs/lspconfig.lua
@@ -55,7 +55,7 @@ local servers = {
     -- clangd = {},
     -- gopls = {},
     -- pyright = {},
-    -- rust_analyzer = {},
+    rust_analyzer = {},
     -- tsserver = {},
     -- html = { filetypes = { "html", "twig", "hbs"} },
 
