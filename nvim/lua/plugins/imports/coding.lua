a0fdde66a1b21dd23d76a44509a2b4103b932036
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2024-05-08 22:49:10 +0200

    chore: migrate indent-blankline to v3

--- nvim/lua/plugins/imports/coding.lua
+++ nvim/lua/plugins/imports/coding.lua
@@ -31,11 +31,10 @@ return {
     -- Add indentation guides even on blank lines
     {
         'lukas-reineke/indent-blankline.nvim',
-        -- Enable `lukas-reineke/indent-blankline.nvim`
+        main = "ibl",
         -- See `:help indent_blankline.txt`
         opts = {
-            char = '┊',
-            show_trailing_blankline_indent = false,
+            indent = { char = "┊" },
         },
     },
 
