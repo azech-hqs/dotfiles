13b576afafaf9bd0ce8d4cc296ae7a6c0fbd27e0
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2024-05-18 16:06:08 +0200

    chore: register keys with which-key

--- nvim/lua/plugins/imports/helpers.lua
+++ nvim/lua/plugins/imports/helpers.lua
@@ -1,4 +1,9 @@
 return {
     -- Show key bindings
-    { "folke/which-key.nvim", opts = {} },
+    {
+        "folke/which-key.nvim",
+        config = function ()
+            require("plugins.configs.which_key")
+        end
+    },
 }
