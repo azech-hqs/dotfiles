3b9dc5abbb56fa397f35c584d84cc78f56e578b6
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2024-05-18 16:57:08 +0200

    feat(fs): add oil.nvim plugin for filesystem operations

---
+++ nvim/lua/plugins/imports/fs.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        'stevearc/oil.nvim',
+        opts = {},
+        -- Optional dependencies
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        keys = {
+            { "-", "<cmd>Oil<cr>", { desc = "Oil: Open parent directory" } }
+        }
+    }
+}
