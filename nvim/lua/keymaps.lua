0884127000f94e2c6cb130889d48862328706265
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2024-06-11 10:32:09 +0200

    feat: add keymaps to resize splits

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -16,3 +16,9 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- Resize splits
+vim.keymap.set("n", "<M-t>", "<c-w>5<", { desc = "Move split to the left" })
+vim.keymap.set("n", "<M-s>", "<c-w>5>", { desc = "Move split to the right" })
+vim.keymap.set("n", "<M-g>", "<C-W>+", { desc = "Move split upwards" })
+vim.keymap.set("n", "<M-h>", "<C-W>->", { desc = "Move split downwards" })
