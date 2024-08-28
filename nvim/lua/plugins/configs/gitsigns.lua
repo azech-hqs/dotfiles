9223ccff1714fa6f2161e012ea565215c192ec18
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2024-08-28 23:12:37 +0200

    chore: do not show status line for staged hunks

--- nvim/lua/plugins/configs/gitsigns.lua
+++ nvim/lua/plugins/configs/gitsigns.lua
@@ -7,6 +7,15 @@ require("gitsigns").setup({
         changedelete = { text = "~" },
         untracked = { text = "┆" },
     },
+    signs_staged = {
+        add = { text = "┃" },
+        change = { text = "┃" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked = { text = "┆" },
+    },
+    signs_staged_enable = false,
     -- stylua: ignore start
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
