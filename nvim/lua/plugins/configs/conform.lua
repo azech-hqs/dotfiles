1bf995366eef1ba09a2056247215cede2502ae01
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-03-29 15:46:33 +0100

    fix(nvim): flatten nested list
    Nested list for formatter sequence not supported anymore.

--- nvim/lua/plugins/configs/conform.lua
+++ nvim/lua/plugins/configs/conform.lua
@@ -1,26 +1,12 @@
 require("conform").setup({
     notify_on_error = false,
-    format_on_save = false,
-    -- function(bufnr)
-    --     -- Disable "format_on_save lsp_fallback" for languages that don't
-    --     -- have a well standardized coding style. You can add additional
-    --     -- languages here or re-enable it for the disabled ones.
-    --     local disable_filetypes = { c = true, cpp = true }
-    --     return {
-    --         timeout_ms = 500,
-    --         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-    --     }
-    -- end,
+    format_on_save = nil,
     formatters_by_ft = {
         lua = { "stylua" },
         go = { "goimports", "gofmt"},
-        -- Conform can also run multiple formatters sequentially
         python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        javascript = { { "prettierd", "prettier" } },
-        typescript = { { "prettierd", "prettier" } },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
     },
 })
 
