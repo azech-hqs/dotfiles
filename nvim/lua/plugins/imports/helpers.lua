02922e52c2674bd4e9787ea7346a0dcba4754079
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-05-24 20:49:14 +0200

    refactor(nvim): use opts and keys for snacks

--- nvim/lua/plugins/imports/helpers.lua
+++ nvim/lua/plugins/imports/helpers.lua
@@ -10,8 +10,7 @@ return {
         "folke/snacks.nvim",
         priority = 1000,
         lazy = false,
-        config = function()
-            require("plugins.configs.snacks")
-        end,
+        opts = require("plugins.configs.snacks").opts,
+        keys = require("plugins.configs.snacks").keys,
     },
 }
