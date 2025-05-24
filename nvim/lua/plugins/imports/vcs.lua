945cbec7ae72a19d7adb71c7afde206eef86ea80
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-05-25 00:02:34 +0200

    refactor(nvim): use opts for gitsigns.nvim

--- nvim/lua/plugins/imports/vcs.lua
+++ nvim/lua/plugins/imports/vcs.lua
@@ -3,9 +3,7 @@ return {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     {
         "lewis6991/gitsigns.nvim",
-        config = function()
-            require("plugins.configs.gitsigns")
-        end,
+        opts = require("plugins.configs.gitsigns"),
     },
     {
         "kdheepak/lazygit.nvim",
