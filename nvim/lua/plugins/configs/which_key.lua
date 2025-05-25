87ae9f4c11ea6bece5793cb9a258101751c7289d
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-05-25 12:48:42 +0200

    chore(nvim): add missing whichkey groups

--- nvim/lua/plugins/configs/which_key.lua
+++ nvim/lua/plugins/configs/which_key.lua
@@ -2,20 +2,26 @@
 local M = {
     -- Document existing key chains
     spec = {
+        { "<leader>b", group = "[B]uffer" },
         { "<leader>c", group = "[C]ode" },
         { "<leader>c_", hidden = true },
         { "<leader>d", group = "[D]ocument" },
         { "<leader>d_", hidden = true },
+        { "<leader>f", group = "[F]ind" },
+        { "<leader>g", group = "[G]oto" },
         { "<leader>h", group = "Git [H]unk" },
         { "<leader>h_", hidden = true },
+        { "<leader>l", group = "[L]azygit" },
         { "<leader>r", group = "[R]ename" },
         { "<leader>r_", hidden = true },
         { "<leader>s", group = "[S]earch" },
         { "<leader>s_", hidden = true },
         { "<leader>t", group = "[T]oggle" },
         { "<leader>t_", hidden = true },
+        { "<leader>u", group = "[U]tils" },
         { "<leader>w", group = "[W]orkspace" },
         { "<leader>w_", hidden = true },
+        { "<leader>z", group = "[Z]k notes" },
         -- visual mode
         { "<leader>h", desc = "Git [H]unk", mode = "v" },
     },
