a87723cc5a42a4e2716f735b7917bdda040fd4cf
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-05-25 12:30:50 +0200

    refactor(nvim): deactivate or remove telescope keymaps

--- nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/lua/plugins/configs/lspconfig.lua
@@ -15,27 +15,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
         map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
         map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-        map("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-        map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-        map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
         map("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-        map(
-            "<leader>ds",
-            require("telescope.builtin").lsp_document_symbols,
-            "[D]ocument [S]ymbols"
-        )
-        map(
-            "<leader>ws",
-            require("telescope.builtin").lsp_dynamic_workspace_symbols,
-            "[W]orkspace [S]ymbols"
-        )
 
         -- See `:help K` for why this keymap
         map("K", vim.lsp.buf.hover, "Hover Documentation")
         map("<leader>k", vim.lsp.buf.signature_help, "Signature Documentation")
 
         -- Lesser used LSP functionality
-        map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
         map("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
         map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
         map("<leader>wl", function()
@@ -77,8 +63,11 @@ require("neodev").setup()
 --  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
 --  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities =
-    vim.tbl_deep_extend("force", capabilities, require('blink.cmp').get_lsp_capabilities(capabilities))
+capabilities = vim.tbl_deep_extend(
+    "force",
+    capabilities,
+    require("blink.cmp").get_lsp_capabilities(capabilities)
+)
 
 -- Ensure language servers and tools are installed via Mason
 require("mason").setup()
