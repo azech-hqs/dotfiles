4ee33c7128aa8bfe2d69406e9c2c95d9a75a5841
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-05-25 12:32:33 +0200

    refactor(nvim): ensure more installed languages for treesitter

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -5,12 +5,16 @@ require("nvim-treesitter.configs").setup({
     -- Add languages to be installed here that you want installed for treesitter
     ensure_installed = {
         "bash",
+        "css",
         "go",
         "html",
         "javascript",
+        "latex",
         "lua",
         "python",
+        "regex",
         "rust",
+        "scss",
         "tsx",
         "typescript",
         "vimdoc",
