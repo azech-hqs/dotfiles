c769dc8677175912502f162bd885cb5c00748eea
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-05-25 12:50:26 +0200

    refactor(nvim): change markdown render toggle keymap
    Move to 'toggle' keymap group.

--- nvim/lua/plugins/configs/render-markdown.lua
+++ nvim/lua/plugins/configs/render-markdown.lua
@@ -1,9 +1,9 @@
-local rmd = require('render-markdown')
+local rmd = require("render-markdown")
 
 rmd.setup({
     completions = { blink = { enabled = true } },
 })
 
-vim.keymap.set("n", "<leader>md", function()
+vim.keymap.set("n", "<leader>tm", function()
     rmd.buf_toggle()
-end, { desc = "Toggle [M]ark[D]own render" })
+end, { desc = "[T]oggle [M]arkdown render" })
