10428c7e2821c39382daa4c822997228425e188c
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-06-09 14:35:37 +0200

    refactor(nvim): remove manual LSP capabilities setup

--- nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/lua/plugins/configs/lspconfig.lua
@@ -55,17 +55,6 @@ local servers = {
     },
 }
 
--- LSP servers and clients communicate which features they support through "capabilities".
---  By default, Neovim supports a subset of the LSP specification.
---  With blink.cmp, Neovim has *more* capabilities which are communicated to the LSP servers.
---  Explanation from TJ: https://youtu.be/m8C0Cq9Uv9o?t=1275
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = vim.tbl_deep_extend(
-    "force",
-    capabilities,
-    require("blink.cmp").get_lsp_capabilities(capabilities)
-)
-
 -- Ensure language servers and tools are installed via Mason
 require("mason").setup()
 
