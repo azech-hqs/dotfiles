d3e2d35e418ef559d044df779babda13408944c3
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-10-17 14:45:40 +0200

    refactor(nvim): enable prettier for react and vue

--- nvim/lua/plugins/configs/conform.lua
+++ nvim/lua/plugins/configs/conform.lua
@@ -9,6 +9,9 @@ M.opts = {
         python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
         javascript = { "prettierd", "prettier", stop_after_first = true },
         typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        vue = { "prettierd", "prettier", stop_after_first = true },
     },
 }
 
