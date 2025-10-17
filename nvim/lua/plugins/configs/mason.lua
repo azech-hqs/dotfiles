82eb83e92162432e15cb08e6733964da0ed78a36
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-10-17 15:50:27 +0200

    refactor(nvim): update ensure_installed for mason
    Removes isort, eslint, gopls. Adds vtsls and vue_ls.

--- nvim/lua/plugins/configs/mason.lua
+++ nvim/lua/plugins/configs/mason.lua
@@ -1,9 +1,9 @@
 -- Ensure language servers and tools are installed via Mason
 require("mason").setup()
 
-require("mason-tool-installer").setup({ ensure_installed = { "prettier", "stylua", "isort" } })
+require("mason-tool-installer").setup({ ensure_installed = { "prettier", "stylua" } })
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "eslint", "gopls", "lua_ls", "pyright", "ruff", "rust_analyzer" },
+    ensure_installed = { "lua_ls", "pyright", "ruff", "rust_analyzer", "vtsls", "vue_ls" },
     automatic_enable = true,
 })
