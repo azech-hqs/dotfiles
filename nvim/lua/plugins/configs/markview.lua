89e3449f9f5202d2892bd54132f48764b9f2ca82
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-10-21 21:38:06 +0200

    refactor(nvim): disable markdown list item padding

--- nvim/lua/plugins/configs/markview.lua
+++ nvim/lua/plugins/configs/markview.lua
@@ -4,6 +4,24 @@ M.opts = {
     preview = {
         icon_provider = "mini", -- "mini", "internal", or "devicons"
     },
+    list_items = {
+        shift_width = 2,
+        marker_minus = {
+            add_padding = false,
+        },
+        marker_plus = {
+            add_padding = false,
+        },
+        marker_star = {
+            add_padding = false,
+        },
+        marker_dot = {
+            add_padding = false,
+        },
+        marker_parentheses = {
+            add_padding = false,
+        },
+    },
 }
 
 M.keys = {}
