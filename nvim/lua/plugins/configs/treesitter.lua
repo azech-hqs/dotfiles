a44186548ab31b3ee970cc993ba5187217412e6a
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-10-30 22:05:12 +0100

    feat(nvim): add keymap to toggle treesitter context

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -112,3 +112,7 @@ require("treesitter-context").setup({
     zindex = 20, -- The Z-index of the context window
     on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
 })
+
+vim.keymap.set("n", "<leader>tc", function()
+    require("treesitter-context").toggle()
+end, { desc = "[T]oggle [C]ontext" })
