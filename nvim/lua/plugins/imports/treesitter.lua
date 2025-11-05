4fc22616e6c50c393863e9aa237cae517190fb10
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-11-05 14:19:41 +0100

    feat(nvim): add nvim-ts-autotag

--- nvim/lua/plugins/imports/treesitter.lua
+++ nvim/lua/plugins/imports/treesitter.lua
@@ -11,4 +11,16 @@ return {
             require("plugins.configs.treesitter")
         end,
     },
+    -- Autorename/autoclose HTML tags
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            local opts = {
+                enable_close = true,
+                enable_rename = true,
+                enable_close_on_slash = true,
+            }
+            require("nvim-ts-autotag").setup({ opts = opts })
+        end,
+    },
 }
