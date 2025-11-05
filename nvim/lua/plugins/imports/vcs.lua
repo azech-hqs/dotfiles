3811a132c9e052f0b3a554fc404f4641ec92d7d2
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2025-11-05 12:38:22 +0100

    refactor(nvim): setup diffview keymaps

--- nvim/lua/plugins/imports/vcs.lua
+++ nvim/lua/plugins/imports/vcs.lua
@@ -6,5 +6,20 @@ return {
     },
     {
         "sindrets/diffview.nvim",
+        lazy = true,
+        cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+        keys = {
+            { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Open diff [v]iew" },
+            { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "Open file [h]istory" },
+            {
+                "<leader>gB",
+                "<cmd>DiffviewOpen origin/HEAD...HEAD --imply-local<cr>",
+                desc = "Review branch changes",
+            },
+        },
+        opts = {
+            enhanced_diff_hl = true,
+            use_icons = true,
+        },
     },
 }
