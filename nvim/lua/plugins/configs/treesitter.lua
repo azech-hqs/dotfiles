6c8556e8df848b9ac47fe53e40519da2bbe275e4
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-02-21 13:43:36 +0100

    refactor(nvim): limit treesitter context window size

--- nvim/lua/plugins/configs/treesitter.lua
+++ nvim/lua/plugins/configs/treesitter.lua
@@ -100,7 +100,8 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 -- Treesitter-context
 require("treesitter-context").setup({
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    multiwindow = false, -- Enable multiwindow support.
+    max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
     min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
     line_numbers = true,
     multiline_threshold = 20, -- Maximum number of lines to show for a single context
