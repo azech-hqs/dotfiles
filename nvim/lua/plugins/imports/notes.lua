881fc55f1acc49a77c464114fa63029bb8a14ebc
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-04-09 23:18:06 +0200

    refactor(nvim): move markview.nvim

--- nvim/lua/plugins/imports/notes.lua
+++ nvim/lua/plugins/imports/notes.lua
@@ -1,12 +1,1 @@
-return {
-    -- Markdown rendering
-    {
-        "OXY2DEV/markview.nvim",
-        lazy = false,
-        dependencies = {
-            "saghen/blink.cmp",
-            "nvim-mini/mini.icons",
-        },
-        opts = require("plugins.configs.markview").opts,
-    },
-}
+return {}
