4ec74b3f225e9fef62ad7c2b6105d36837c5bb4f
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-04-09 14:09:54 +0200

    refactor(nvim): move gitsigns.nvim

--- nvim/lua/plugins/imports/vcs.lua
+++ nvim/lua/plugins/imports/vcs.lua
@@ -1,9 +1,4 @@
 return {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = require("plugins.configs.gitsigns"),
-    },
     {
         "sindrets/diffview.nvim",
         lazy = true,
