9115c8deb27fda2c878baa157bcbfc0367ba36c0
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-04-09 13:57:46 +0200

    refactor(nvim): move snacks.nvim

--- nvim/lua/plugins/imports/helpers.lua
+++ nvim/lua/plugins/imports/helpers.lua
@@ -1,10 +1,1 @@
-return {
-    -- QoL improvements
-    {
-        "folke/snacks.nvim",
-        priority = 1000,
-        lazy = false,
-        opts = require("plugins.configs.snacks").opts,
-        keys = require("plugins.configs.snacks").keys,
-    },
-}
+return {}
