e568241fe8c110e41dc6ddeafab2eabd2fb5fd01
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-04-09 14:03:53 +0200

    refactor(nvim): move todo-comments.nvim

--- nvim/plugin/01-dependencies.lua
+++ nvim/plugin/01-dependencies.lua
@@ -2,6 +2,7 @@ vim.pack.add({
     "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/rafamadriz/friendly-snippets",
     "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/nvim-lua/plenary.nvim",
 })
 
 require("nvim-web-devicons").setup()
