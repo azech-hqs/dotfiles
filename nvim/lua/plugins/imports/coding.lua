d1f2a587a2b7994ad16508a7d056e44ef8b35ad7
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-04-11 14:08:09 +0200

    refactor(nvim): move mini.surround

--- nvim/lua/plugins/imports/coding.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    {
-        "nvim-mini/mini.surround",
-        version = false,
-        opts = require("plugins.configs.mini_surround").opts,
-    },
-}
--- nvim/plugin/mini.lua
+++ nvim/plugin/mini.lua
@@ -2,3 +2,16 @@ vim.pack.add({ "https://github.com/nvim-mini/mini.nvim" })
 
 require("mini.cursorword").setup()
 require("mini.icons").setup()
+require("mini.surround").setup({
+    mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual modes
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find surrounding (to the right)
+        find_left = "gsF", -- Find surrounding (to the left)
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+        suffix_last = "gl", -- Suffix to search with "prev" method
+        suffix_next = "gn", -- Suffix to search with "next" method
+    },
+})
