bcb57f765b96eacd26c66e377221c72a7ae76c4d
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-04-11 13:26:47 +0200

    chore(nvim): remove unused lazy imports and configs

--- nvim/lua/plugins/configs/mason.lua
+++
@@ -1,9 +0,0 @@
--- Ensure language servers and tools are installed via Mason
-require("mason").setup()
-
-require("mason-tool-installer").setup({ ensure_installed = { "prettier", "stylua" } })
-
-require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "pyright", "ruff", "rust_analyzer", "vtsls", "vue_ls" },
-    automatic_enable = true,
-})
