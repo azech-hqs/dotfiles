c255566c8537564612677d1ad0dd5713262a7849
Alexander Zech <10773967+azech-hqs@users.noreply.github.com>
2026-04-11 11:46:57 +0200

    refactor(nvim): move nvim-ts-autotag

--- nvim/lua/plugins/imports/treesitter.lua
+++ nvim/lua/plugins/imports/treesitter.lua
@@ -1,14 +1,1 @@
-return {
-    -- Autorename/autoclose HTML tags
-    {
-        "windwp/nvim-ts-autotag",
-        config = function()
-            local opts = {
-                enable_close = true,
-                enable_rename = true,
-                enable_close_on_slash = true,
-            }
-            require("nvim-ts-autotag").setup({ opts = opts })
-        end,
-    },
-}
+return {}
